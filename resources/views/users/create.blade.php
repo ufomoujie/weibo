357a91b48371b7e1a1e5598adef2f2bd8caa5f1c
mou <ufomoujie@gmail.com>
2021-06-22 13:49:27 +0900

    add CSRF check

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,6 +9,7 @@
       </div>
       <div class="card-body">
         <form method="post" action=" {{ route('users.store') }}">
+          {{ csrf_field() }}
           <div class="form-group">
             <label for="name">name: </label>
             <input id="name" type="text" name="name" class="form-control" value="{{ old('name') }}">
@@ -28,6 +29,8 @@
             <label for="password_confirmation">password_confirmation: </label>
             <input id="password_confirmation" type="text" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}" >
           </div>
+
+          <button type="submit" class="btn btn-primary">sign up</button>
         </form>
       </div>
     </div>
