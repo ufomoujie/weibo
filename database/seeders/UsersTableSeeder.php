833d359145b468698e51a8489d2b5b46e1464241
mou <ufomoujie@gmail.com>
2021-07-12 14:57:59 +0900

    list users

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        User::factory(50)->create();
+
+        $user = User::find(1);
+        $user->name = 'mou';
+        $user->email = 'mou@eazesystems.com';
+        $user->save();
+    }
+}
