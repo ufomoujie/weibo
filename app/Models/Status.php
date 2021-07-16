1a5608bebf9559d4e4ae6c0adca06a27b1411bb8
mou <ufomoujie@gmail.com>
2021-07-16 12:10:01 +0900

    submit weibo

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -8,7 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Status extends Model
 {
     use HasFactory;
-
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'content',
+    ];
+    
     public function user() {
         return $this->belongsTo(User::class);
     }
